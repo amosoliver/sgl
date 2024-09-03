7b5cffa00eaf1792198c95d316a2e475e17a20e1
amos_oliveira <oliveramos13542@gmail.com>
2024-09-03 11:35:44 -0400

    wip

--- app/models/literatura.rb
+++ app/models/literatura.rb
@@ -3,22 +3,26 @@ class Literatura < ApplicationRecord
 
   def self.relatorio
     sql = <<-SQL
-      SELECT
+    SELECT
         congregacoes.descricao AS congregacao,
         literaturas.trimestre AS trimestre_literatura,
-        COALESCE(entradas.trimestre, literaturas.trimestre) AS trimestre_entrada,
         literaturas.ano,
         literaturas.valor AS valor_a_pagar,
         COALESCE(SUM(entradas.valor), 0) AS valor_pago,
         (literaturas.valor - COALESCE(SUM(entradas.valor), 0)) AS valor_pendente
-      FROM literaturas
-      INNER JOIN congregacoes ON literaturas.congregacao_id = congregacoes.id
-      LEFT JOIN entradas ON entradas.congregacao_id = congregacoes.id
-      GROUP BY
+    FROM literaturas
+    INNER JOIN congregacoes ON literaturas.congregacao_id = congregacoes.id
+    LEFT JOIN entradas 
+        ON entradas.congregacao_id = congregacoes.id
+        AND entradas.trimestre = literaturas.trimestre
+        AND entradas.ano = literaturas.ano
+        WHERE literaturas.trimestre = 3
+    GROUP BY
         congregacoes.descricao,
         literaturas.trimestre,
         literaturas.ano,
         literaturas.valor
+
     SQL
   
     ActiveRecord::Base.connection.execute(sql)
